b1ce1e8d15ed57f112352783396d9acfbdd709ae
Max Tiedl <max.tiedl@stud.uni-heidelberg.de>
2023-11-22 17:35:12 +0100

    Implement Special Character Treatment

--- src/layout_extractor.py
+++ src/layout_extractor.py
@@ -10,21 +10,26 @@ class LayoutExtractor:
     def find_columns(self, max_diff, symbols):
         chars = sorted(self.clipping.chars, key=lambda e: e['x0'])
         separator = []
-        symbol_separator = []
         i=0
         while i < len(chars)-1:
+            char = chars[i+1]
             # remove white spaces
-            if chars[i+1]['text'] == ' ':
+            if char['text'] == ' ':
                 chars.pop(i+1)
             else:
-                diff = chars[i+1]['x0'] - chars[i]['x1']
+                diff = char['x0'] - chars[i]['x1']
                 if diff > max_diff:
-                    separator.append(chars[i+1]['x0'])
-                elif chars[i+1]['text'] in symbols or chars[i]['text'] in symbols:
-                    symbol_separator.append(chars[i+1]['x0'])
+                    separator.append(((char['x0'], self.table['bbox'][1]), (char['x0'], self.table['bbox'][3])))
+                #elif chars[i+1]['text'] in symbols or chars[i]['text'] in symbols:
+                #    symbol_separator.append(chars[i+1]['x0'])
+                if chars[0]['text'] in symbols or char['text'] in symbols:
+                    print(char['text'])
+                    top, bottom = self.find_unit_column(chars[i])
+                    separator.append(((char['x0'], top), (char['x0'], bottom)))
+                    separator.append(((char['x1'], top), (char['x1'], bottom)))
                 i+=1
 
-        return separator        
+        return separator
 
     def find_rows(self, max_diff):
         chars = sorted(self.clipping.chars, key=lambda e: e['top'])
@@ -43,10 +48,33 @@ class LayoutExtractor:
                 i+=1
 
         return separator  
+    
+    def find_unit_column(self, char):
+        lines = sorted(self.table['lines'], key=lambda e: e['top'])
+        lines.append({'top': self.table['bbox'][3], 'bottom': self.table['bbox'][3]})
+
+        for i in range(len(lines)-1):
+            print(f"{char['bottom']}\t{lines[i]['top']}\t{lines[i+1]['bottom']}")
+            if char['top'] >= lines[i]['bottom'] and char['bottom'] <= lines[i+1]['top']:
+                return lines[i]['bottom'], lines[i+1]['top']
+            
+        return self.table['bbox'][1], self.table['bbox'][3]
+
+    #def unit_layout(self, lines):
+    #    header_lines = sorted(self.table['lines'], key=lambda e: e['top'])
+#
+    #    i=0
+    #    while i < len(lines):
+    #        self.clipping.crop([lines[i], 0, lines[i+1], self.clipping.height])
+    #        i+=2
+    #        e = [x for x in header_lines: x['']
+
 
     def find_layout(self, x_space, y_space, symbols):
         column_separator = self.find_columns(x_space, symbols)
         row_separator = self.find_rows(y_space)
+
+        #self.unit_layout(symbol_separator)
 
         return column_separator, row_separator
 
@@ -79,7 +107,7 @@ def pdfplumber_table_extraction(table, table_clip):
 
 if __name__ == '__main__':
 
-    with pdfplumber.open("examples/pdf/FDX/2017/page_62.pdf") as pdf:
+    with pdfplumber.open("examples/pdf/FDX/2017/page_26.pdf") as pdf:
         page = pdf.pages[0]
         t_finder = TableFinder(page)
         tables = t_finder.find_tables()
@@ -88,7 +116,7 @@ if __name__ == '__main__':
     le = LayoutExtractor(tables[0], table_clip)
     column_separator, row_separator = le.find_layout(5, 2, ['$', '%']) # first value to 3 for separating dollar signs and to 0.01 for separating also percent signs
     im = table_clip.to_image(resolution=300)
-    im.draw_vlines(column_separator, stroke_width=2)
+    im.draw_lines(column_separator, stroke_width=2)
     im.draw_hlines(row_separator, stroke_width=2)
     im.save('table.png')
 
