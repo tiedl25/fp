d15700e007acc52fc0fd5a65727cae6c62ebc783
tiedl25 <max.tiedl@gmx.de>
2023-12-13 17:56:44 +0100

    Use multiprocessing for testing

--- test/test_fintab.py
+++ test/test_fintab.py
@@ -6,6 +6,9 @@ import numpy as np
 import os
 import json
 import pdfplumber
+import threading
+import concurrent.futures
+import multiprocessing
 
 def getPdfPaths(path):
     path = path + '/pdf'
@@ -21,21 +24,16 @@ def extractAnnotatedTables(path):
     with open(f"{path}/FinTabNet_1.0.0_table_test.jsonl") as f:
         test_dataset = [json.loads(line) for line in f]
         
+    
     test_dataset.sort(key = lambda e: e['filename'])
+    #test_dataset = test_dataset[0:1000]
 
     total = len(test_dataset)
-
-    page_height = 792
 
     # group tables in examples json file by their filename and sort their coordinates to meet the requirements of pdfplumber
     test_tables_grouped = []
 
     for table in test_dataset:
-        bbox = table['bbox']
-        bbox3 = bbox[3]
-        bbox[3] = page_height - bbox[1]
-        bbox[1] = page_height - bbox3
-
         if len(test_tables_grouped) > 0 and table['filename'] == test_tables_grouped[len(test_tables_grouped)-1]['filename']:
             test_tables_grouped[len(test_tables_grouped)-1]['tables'].append({
                 'bbox': table['bbox'],
@@ -46,6 +44,64 @@ def extractAnnotatedTables(path):
 
     return test_tables_grouped, total
 
+def test(pdf_paths, annotated_tables):
+    matches = 0
+    i = 0
+    tol = 5
+
+    for pdf_path in pdf_paths:
+        if i >= len(annotated_tables):
+            break
+        # Skip tables in one of both sets if they doesn't share the same filename
+        if pdf_path != annotated_tables[i]['filename']:
+            continue
+        
+        #tableExtractor = TableExtractor(path=f"{dataset_path}/pdf/{pdf_path}", separate_units=False)
+        #tables = tableExtractor.extractTables(page_index=0) # all pdfs contain only one page
+        #page = tableExtractor.pages[0]
+
+        with pdfplumber.open(f"{dataset_path}/pdf/{pdf_path}") as pdf:
+            page = pdf.pages[0]
+            tableExtractor = TableFinder(page)
+            tables = tableExtractor.find_tables(left_threshold=20, right_threshold=20)
+
+        # Check if there are any tables
+        if len(tables) > 0 or len(annotated_tables[i]['tables']) > 0:
+            #im = page.to_image(resolution=300)
+
+            test_tables = annotated_tables[i]['tables']
+
+            for test_table in test_tables:
+                bbox = test_table['bbox']
+                bbox3 = bbox[3]
+                bbox[3] = page.height - bbox[1]
+                bbox[1] = page.height - bbox3
+
+            for t_i, table in enumerate(tables):
+                match = False
+                for test_table in test_tables:
+                    #assert_horizontal = abs(table['bbox'][1] - test_tables[j]['bbox'][1]) < tol and abs(table['bbox'][3] - test_tables[j]['bbox'][3]) < tol
+
+                    assert_all = np.allclose(table['bbox'], test_table['bbox'], atol=20)
+
+                    if assert_all:
+                        match = True
+                        matches += 1                        
+
+                #if not match:
+                #    print(f"\t{pdf_path} Table {t_i+1}")
+                #else:
+                #    print(f"\t\t{pdf_path} Table {t_i+1}")
+
+            #bboxs = [table['bbox'] for table in test_tables]
+            #im.draw_rects(bboxs, stroke_width=0, fill=(230, 65, 67, 65)) # red for test tables
+            #im.draw_rects([x['bbox'] for x in tables], stroke_width=0)
+            #im.save(f"img/{pdf_path.replace('/', '_')[0:-4]}.png")
+                
+            i+=1
+
+    return matches
+
 if __name__ == '__main__':
     dataset_path = "fintabnet"
     pdf_paths = getPdfPaths(dataset_path)
@@ -54,57 +110,26 @@ if __name__ == '__main__':
 
     pdf_paths.sort()
 
-    matches = 0
-
     print("Doesn't match:")
 
-    i = 0
-    tol = 5
+    batch_number = 500
+    batches = int(total / batch_number)
+    thread = []
+    total_matches = 0
 
-    annotated_tables = annotated_tables[0:10]
+    #total_matches = test(pdf_paths, annotated_tables)
 
-    for pdf_path in pdf_paths:
-        if i >= len(annotated_tables):
-            break
-        # Skip tables in one of both sets if they doesn't share the same filename
-        if pdf_path != annotated_tables[i]['filename']:
-            continue
-        #print(pdf_path)
-        #tableExtractor = TableExtractor(path=f"{dataset_path}/pdf/{pdf_path}", separate_units=False)
-        #tables = tableExtractor.extractTables(page_index=0) # all pdfs contain only one page
-        #page = tableExtractor.pages[0]
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        matches = [executor.submit(test, pdf_paths, annotated_tables[i*batch_number:(i+1)*batch_number]) for i in range(batches)]
+        for m in matches:
+            total_matches += m.result()
 
-        with pdfplumber.open(f"{dataset_path}/pdf/{pdf_path}") as pdf:
-            page = pdf.pages[0]
-            tableExtractor = TableFinder(page)
-            tables = tableExtractor.find_tables(left_threshold=10, right_threshold=10)
+    #for i in range(batches):
+    #    thread.append(multiprocessing.Process(target=test, args=(pdf_paths, annotated_tables[i*200:(i+1)*200],)))
+    #    thread[i].start()
+#
+    #for t in thread:
+    #    t.join()
+    #    total_matches += t.result()
 
-        # Check if there are any tables
-        if len(tables) > 0 or len(annotated_tables[i]['tables']) > 0:
-            im = page.to_image(resolution=300)
-
-            test_tables = annotated_tables[i]['tables']
-
-            for t_i, table in enumerate(tables):
-                match = False
-                for test_table in test_tables:
-                    #assert_horizontal = abs(table['bbox'][1] - test_tables[j]['bbox'][1]) < tol and abs(table['bbox'][3] - test_tables[j]['bbox'][3]) < tol
-                    assert_all = np.allclose(table['bbox'], test_table['bbox'], atol=20)
-
-                    if assert_all:
-                        match = True
-                        matches += 1                        
-
-                if not match:
-                    print(f"\t{pdf_path} Table {t_i+1}")
-                else:
-                    print(f"\t\t{pdf_path} Table {t_i+1}")
-
-            bboxs = [table['bbox'] for table in test_tables]
-            im.draw_rects(bboxs, stroke_width=0, fill=(230, 65, 67, 65)) # red for test tables
-            im.draw_rects([x['bbox'] for x in tables], stroke_width=0)
-            im.save(f"img/{os.path.basename(pdf_path)[0:-4]}.png")
-                
-            i+=1
-        
-    print(f"Matches: {matches}/{total}\t{matches/total*100} %")
+    print(f"Matches: {total_matches}/{total}\t{total_matches/total*100} %")
